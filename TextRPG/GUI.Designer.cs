5de1f06c13eacd1f6f960db35a978781c5e076d0
Tamara Tanasescu <gokimster@yahoo.com>
2015-11-16 14:36:44 +0000

    Initial GUI commit

---
+++ TextRPG/GUI.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TextRPG
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.inputPanel = new System.Windows.Forms.Panel();
+            this.input = new System.Windows.Forms.TextBox();
+            this.inputLocationLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mainPanel.SuspendLayout();
+            this.inputPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoSize = true;
+            this.mainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mainPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mainPanel.Controls.Add(this.textBox1);
+            this.mainPanel.Controls.Add(this.inputPanel);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(776, 460);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // inputPanel
+            // 
+            this.inputPanel.AutoSize = true;
+            this.inputPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.inputPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputPanel.Controls.Add(this.inputLocationLabel);
+            this.inputPanel.Controls.Add(this.input);
+            this.inputPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.inputPanel.Location = new System.Drawing.Point(0, 423);
+            this.inputPanel.Name = "inputPanel";
+            this.inputPanel.Size = new System.Drawing.Size(776, 37);
+            this.inputPanel.TabIndex = 2;
+            // 
+            // input
+            // 
+            this.input.BackColor = System.Drawing.Color.Black;
+            this.input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.input.Font = new System.Drawing.Font("Power Red and Green", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.ForeColor = System.Drawing.Color.White;
+            this.input.Location = new System.Drawing.Point(27, 6);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(739, 26);
+            this.input.TabIndex = 1;
+            // 
+            // inputLocationLabel
+            // 
+            this.inputLocationLabel.AutoSize = true;
+            this.inputLocationLabel.BackColor = System.Drawing.Color.Transparent;
+            this.inputLocationLabel.Font = new System.Drawing.Font("Power Red and Green", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLocationLabel.ForeColor = System.Drawing.Color.White;
+            this.inputLocationLabel.Location = new System.Drawing.Point(5, 6);
+            this.inputLocationLabel.Name = "inputLocationLabel";
+            this.inputLocationLabel.Size = new System.Drawing.Size(22, 25);
+            this.inputLocationLabel.TabIndex = 2;
+            this.inputLocationLabel.Text = ">";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(6, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(760, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 460);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "GUI";
+            this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.inputPanel.ResumeLayout(false);
+            this.inputPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Panel inputPanel;
+        private System.Windows.Forms.Label inputLocationLabel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
